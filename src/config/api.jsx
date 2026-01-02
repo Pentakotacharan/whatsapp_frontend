d25b8fdeb73df90bd03fd888b76e4fcb0a7b769b
Pentakotacharan <charanpentakota2006@gmail.com>
2026-01-02 19:24:23 +0530

    add

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://whatsapp-backend-rho-sepia.vercel.app//api", // Your Backend URL
+  baseURL: "https://whatsapp-backend-rho-sepia.vercel.app/api", // Your Backend URL
   headers: {
     "Content-Type": "application/json",
   },
